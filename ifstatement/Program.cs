63cfb13a000d96a4e4a7f591d432e10f9378f14a
Alazarussom <49820916+Alazarussom@users.noreply.github.com>
2019-04-22 15:30:16 -0400

    initial

---
+++ ifstatement/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ifstatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int X=10;
+
+
+
+            if (X > 0)
+            {
+                Console.WriteLine("It is a Negative Number");
+
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
